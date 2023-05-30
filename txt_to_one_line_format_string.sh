49f2a1ff184b5213a718a3cf9d7c0f381ca0ae4a
Randy <tangdengcheng@gmail.com>
2023-05-30 16:18:18 -0700

    ci: chore: clear ticket file, test ticket id: test-32324

--- txt_to_one_line_format_string.sh
+++ txt_to_one_line_format_string.sh
@@ -5,11 +5,12 @@
 # txt should not contain duplicates line.
 one_line_format_string=""
 txt_file=$1
+delimiter=$2
 while IFS= read -r line; do
     if [[ $line == $(tail -n1 "$txt_file") ]]; then
         one_line_format_string="${one_line_format_string}${line}"
     else
-        one_line_format_string="${one_line_format_string}${line},"
+        one_line_format_string="${one_line_format_string}${line}${delimiter}"
     fi
 done <"$txt_file"
 echo "${one_line_format_string}"
